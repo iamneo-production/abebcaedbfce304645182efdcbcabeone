854bd73f94c99335525aee5b2d45e456e80016b3
Iamneo <techadmin@iamneo.ai>
2023-09-10 07:17:22 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,20 +1,23 @@
+// Initial game state
 let gameState = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let gameActive = true;
-let result = document.querySelector('#result');
+let result = document.querySelector('#result'); // Updated to select the result by ID
 let btns = document.querySelectorAll('.btn');
 
-function playerTurn(index) {
+// Function to handle player moves
+const ticTacToe = (element, index) => {
     if (gameState[index] === '' && gameActive) {
         gameState[index] = currentPlayer;
-        btns[index].value = currentPlayer;
-        btns[index].disabled = true;
+        element.value = currentPlayer;
+        element.disabled = true;
         checkResult();
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         result.textContent = `Player ${currentPlayer}'s Turn`;
     }
-}
+};
 
+// Winning combinations
 const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -26,11 +29,12 @@ const winningCombinations = [
     [2, 4, 6],
 ];
 
+// Function to check the game result
 function checkResult() {
     for (const combination of winningCombinations) {
         const [a, b, c] = combination;
         if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
-            result.textContent = `Player ${currentPlayer} Wins!`;
+            result.textContent = `Player ${gameState[a]} Wins!`; // Updated this line
             gameActive = false;
             return;
         }
@@ -41,19 +45,20 @@ function checkResult() {
     }
 }
 
+// Function to reset the game
 function resetGame() {
     currentPlayer = 'X';
     gameActive = true;
     gameState = ['', '', '', '', '', '', '', '', ''];
-    btns.forEach((btn) => {
-        btn.value = '';
-        btn.disabled = false;
-    });
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].value = '';
+        btns[i].disabled = false;
+    }
     result.textContent = `Player ${currentPlayer}'s Turn`;
 }
 
 btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => playerTurn(i));
+    btn.addEventListener('click', () => ticTacToe(btn, i));
 });
 
-document.querySelector('#reset-btn').addEventListener('click', resetGame);
+document.querySelector('#reset-btn').addEventListener('click', resetGame); // Updated to select the reset button by ID
